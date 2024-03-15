27d0118804748d8e4ae77ae596994c58ac302f23
epales <epales@naver.com>
2024-03-15 09:27:28 +0900

    first commit

---
+++ coconamu-backend/src/main/java/com/spring/react/coconamubackend/CoconamuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring.react.coconamubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoconamuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoconamuBackendApplication.class, args);
+	}
+
+}
---
+++ coconamu-backend/src/test/java/com/spring/react/coconamubackend/CoconamuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.react.coconamubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoconamuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
