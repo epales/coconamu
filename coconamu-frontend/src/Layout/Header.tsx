8f564d73676ef1fecb2da11f0cec1103075353c8
epales <epales@naver.com>
2024-03-19 17:40:27 +0900

    UPDATE_0.0.1

--- coconamu-frontend/src/Layout/Header.tsx
+++ coconamu-frontend/src/Layout/Header.tsx
@@ -1,10 +1,18 @@
 function Header() {
     return (
         <div>
-            {/* <img src =""/> 로고 이미지 추가할것 */}
-            {/* 카테고리 */}
-            
-            <input placeholder="검색어"></input>
+            <div>
+                {/* <img src =""/> 로고 이미지 추가할것 */}
+            </div>
+            <div>
+                {/* 카테고리 */}    
+            </div>
+            <div>
+                <input type="text" placeholder="검색어"></input>
+            </div>
+            <div>
+
+            </div>
             {/* 로그인 / 회원가입 / 고객센터 */} 
         </div>
     );
